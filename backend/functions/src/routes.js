f75455124984982f55b618f8d16b1a532af6ca54
João Lucas Mota <joaomota59@hotmail.com>
2021-05-19 18:43:54 -0300

    finalizando a issue #5

--- backend/functions/src/routes.js
+++ backend/functions/src/routes.js
@@ -4,6 +4,12 @@ const routes = express.Router()
 
 const imovelController = require('./controllers/imovelController')
 
+const usuarioConfigController = require('./controllers/usuarioConfigController')
+
 routes.get('/listaImoveis',imovelController.index)
+
+routes.get('/usuarioConfig/:cpf',usuarioConfigController.index)
+
+routes.post('/usuarioConfig/:cpf',usuarioConfigController.setPlano)
 
 module.exports = routes

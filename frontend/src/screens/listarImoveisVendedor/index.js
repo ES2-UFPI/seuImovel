e3c5f68eb0937cb76fd24536e3c6a7bb490d9773
michael <mlmbufpi@gmail.com>
2021-07-05 15:40:18 -0300

    tem um imovel ficticio #118

--- frontend/src/screens/listarImoveisVendedor/index.js
+++ frontend/src/screens/listarImoveisVendedor/index.js
@@ -1,26 +1,62 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TouchableOpacity, Image } from 'react-native'
 import Constants from 'expo-constants'
+import styles from './style'
 
 import api from '../../services/api'
 
 const index = () => {
 
-
+   const [listaImoveis, setListaImoveis] = React.useState([{
+    id: "0LSRd5uFruysZ5K1ROy7",
+    banheiros:3,
+    complemento :"Proximo ao Posto são José teste",
+    cpf :"41789623615",
+    descricao:"Casa com piscina e bem localizada",
+    dimensao: 500,
+    imagens: "https://s2.glbimg.com/l5tf5ALrBpZgm6SyiYv55yoUlh0=/620x413/smart/e.glbimg.com/og/ed/f/original/2020/01/20/leve-e-iluminada-esta-casa-na-bahia-mistura-estrutura-metalica-madeira-e-vidro_9.jpg",
+    latitude :-5.096235980369862,
+    longitude: -42.83879868686199,
+    numero:8877,
+    proprietario:"Joaquim",
+    quartos: 4,
+    tipo: "Venda",
+    valor:387482
+   }])
 
 
 
     return (
-       <SafeAreaView style={styles.container}>
-           <Text>Seus imoveis</Text>
-       </SafeAreaView>
+        <View style={styles.container}>
+        <FlatList 
+            // onEndReached={loadListMovel}
+            onEndReachedThreshold={0.2}
+            showsVerticalScrollIndicator={true}
+            data={listaImoveis}
+            keyExtractor={item => String(item.id)}
+            renderItem={({item}) =>{
+                return(
+                    // <TouchableOpacity onPress={() => navigateToDescricao(item)}>
+                        <View style={styles.contextHomes}>
+                            <Text style={styles.contextText}>
+                                <Text style={styles.title}>{'Casa - '+ item.tipo+ ' (R$' + item.valor+')\n'}</Text> 
+                                <Text style={styles.description}>{item.descricao.substr(0, 50)+'...\n'}</Text> 
+                                <Text style={styles.endereco}>{item.complemento}</Text>
+                            </Text>
+                            <Image
+                            style={styles.imageHome}
+                            source={{uri: item.imagens}}
+                            />
+                        </View>
+                    // </TouchableOpacity>
+                                      
+            )}}        
+        >
+        </FlatList>
+        {/* <Feather onPress={() => navigateToImoveis()} name="map" size={30} style={styles.icon}/> */}
+    </View>
     )
 }
 
 export default index
 
-const styles = StyleSheet.create({
-    container: {
-        marginTop: Constants.statusBarHeight + 10
-    }
-})
---
+++ frontend/src/screens/listarImoveisVendedor/style.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: Constants.statusBarHeight + 10,
+    },
+    contextHomes:{
+        height: 120,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        marginVertical: 10,
+        marginHorizontal: 8,
+        padding: 15,
+        borderBottomWidth: 2,
+        borderColor: 'grey'
+
+    },
+    contextText:{
+        width: '80%'
+    },  
+    title:{
+        fontSize: 22,
+        fontWeight: 'bold'
+    },
+    description:{
+        fontSize: 18,
+        opacity: 0.7
+    },
+    endereco:{
+        fontSize: 14,
+        opacity: 0.8,
+        color: 'green'
+    },
+    imageHome: {
+        width: 80,
+        height: 80,
+        borderRadius: 12,
+    },
+    icon:{
+        color: 'green',
+        position: 'absolute',
+        right: 20,
+        bottom: 20,   
+    }
+})
+
+export default styles
